dd945777bda2c47b77b5676177e0332283281fba
adamhungerford <79060679+adamhungerford@users.noreply.github.com>
2022-02-16 08:45:00 -0500

    Grid set up

---
+++ src/Grid.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class Grid extends Component{
+    render(){
+        return(
+        <>
+            <div className="grid">
+                <div className="portrait">
+                    <div className="portrait-content">
+                        <img src="C:/users/adhun/npclib/public/img/placeholder.png"></img>
+                    </div>
+                </div>
+            </div>
+        </>
+        );
+    }
+}
+
+export default Grid;
