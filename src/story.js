d7c41a0e60f4d9c446554a9574cdbc41c07652e8
adamhungerford <79060679+adamhungerford@users.noreply.github.com>
2022-03-16 08:52:44 -0400

    mathing recap

---
+++ src/story.js
@@ -0,0 +1,11 @@
+function story(){
+    const Story = {"Story":[
+        {"actName": "Act 1: Wideseal", "sessions":[
+            {"recap": "a", "stinger":"b"}
+        ]}
+    ]}
+
+    return Story;
+}
+
+export default story;
