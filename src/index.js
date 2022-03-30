871c9a61cc57821be0f9ccfcb10ac23db31fa679
adamhungerford <79060679+adamhungerford@users.noreply.github.com>
2022-03-30 08:37:08 -0400

    Updating desktop to sass and vars sass file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './desktop.css';
+import './css/index.css';
+import './css/desktop.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
